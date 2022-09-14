de517b903935f2d99b29681d586aed80e80e0a8a
Ante Milina <ante.milina@hotmail.com>
2022-09-14 22:59:03 +0200

    Change id generation to SEQUENCE

--- src/main/java/com/example/demo/domain/Location.java
+++ src/main/java/com/example/demo/domain/Location.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Location {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private Integer postalCode;
